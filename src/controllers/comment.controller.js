5121545eb8a552e55b97e0d31416505eef8aab5d
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-03-01 22:06:57 +0800

    add completed comment controllers and routes and partially finished like controller and routes to codebase

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,25 +1,127 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { parse } from "dotenv";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  const pageNumber = parseInt(page);
+  const limitNumber = parseInt(limit);
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId.");
+  } else if (isNaN(pageNumber) || isNaN(limitNumber)) {
+    throw new ApiError(400, "Invalid page or limit provided."); // isNaN is the MOST RELIABLE WAY TO CHECK IF SOMETHING IS NOT A NUMBER. do not use !parseInt(something) as 0 is considered falsy and will actually fail that test.
+  }
+
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $sort: { createdAt: -1, _id: -1 }, // pagination without sort is unstable, so we add a sort here. this is because without sort, mongoDB returns the documents in an arbitrary order.
+      // SORT BEFORE PAGINATION.
+    },
+    {
+      $skip: (pageNumber - 1) * limitNumber,
+    },
+    {
+      $limit: limitNumber,
+    },
+    {
+      $project: {
+        __v: 0,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, comments, "Video comments retrieved successfully."),
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId.");
+  }
+
+  const newComment = await Comment.create({
+    video: new mongoose.Types.ObjectId(videoId),
+    content: content,
+    commentedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const commentUploaded = await Comment.findById(newComment._id);
+
+  if (!commentUploaded) {
+    throw new ApiError(404, "Error uploading comment.");
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, commentUploaded, "Comment uploaded successfully."),
+    );
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId.");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    { content },
+    { new: true },
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(404, "Comment not found.");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedComment, "Comment updated successfully."),
+    );
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId.");
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(404, "Comment not found.");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, deletedComment, "Comment deleted successfully."),
+    );
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,24 +3,147 @@ import { Like } from "../models/like.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.models.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: toggle like on video
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId.");
+  }
+
+  // if there's a like on the video already, just delete the document
+  const likeToDelete = await Like.findOneAndDelete({
+    video: new mongoose.Types.ObjectId(videoId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  if (likeToDelete) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, likeToDelete, "Video unliked successfully."));
+  }
+
+  // else create a new like document
+  const likeToVideo = await Like.create({
+    video: new mongoose.Types.ObjectId(videoId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const likeCreated = await Like.findById(likeToVideo._id);
+
+  if (!likeCreated) {
+    throw new ApiError(404, "Error liking video.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, likeCreated, "Video liked successfully."));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   //TODO: toggle like on comment
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId.");
+  }
+
+  // if there's a like on the comment already, just delete the document
+  const likeToDelete = await Like.findOneAndDelete({
+    comment: new mongoose.Types.ObjectId(commentId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  if (likeToDelete) {
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, likeToDelete, "Comment unliked successfully."),
+      );
+  }
+
+  // else create a new like document
+  const likeToComment = await Like.create({
+    comment: new mongoose.Types.ObjectId(commentId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const likeCreated = await Like.findById(likeToComment._id);
+
+  if (!likeCreated) {
+    throw new ApiError(404, "Error liking comment.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, likeCreated, "Comment liked successfully."));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   //TODO: toggle like on tweet
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweetId.");
+  }
+
+  // if there's a like on the tweet already, just delete the document
+  const likeToDelete = await Like.findOneAndDelete({
+    tweet: new mongoose.Types.ObjectId(tweetId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  if (likeToDelete) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, likeToDelete, "Tweet unliked successfully."));
+  }
+
+  // else create a new like document
+  const likeToTweet = await Like.create({
+    tweet: new mongoose.Types.ObjectId(tweetId),
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const likeCreated = await Like.findById(likeToTweet._id);
+
+  if (!likeCreated) {
+    throw new ApiError(404, "Error liking tweet.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, likeCreated, "Tweet liked successfully."));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
+
+  const likedVideos = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videosLiked",
+      },
+    },
+    {
+      $unwind: "videosLiked",
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, likedVideos, "Liked videos fetched successfully."),
+    );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
